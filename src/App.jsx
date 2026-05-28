fbea636afb1b061cc78c666b351849ebc22a0493
Romain <romain@mbprom1.home>
2026-05-28 17:33:48 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Form from "./Components/Form";
+import StepTwo from "./Components/StepTwo";
+import { useState } from "react";
 
-function App() {
+function App({ show, username, email, password }) {
+  const [resultForm, setResultForm] = useState(show);
+  console.log(username);
   return (
     <div className="container">
-      <Form />
+      {!resultForm && <Form />}
+      {resultForm && (
+        <StepTwo username={username} email={email} password={password} />
+      )}
     </div>
   );
 }
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import StepTwo from "./StepTwo";
+import App from "../App";
 
 const Form = ({ editForm }) => {
   const [username, setUsername] = useState("");
@@ -12,80 +13,81 @@ const Form = ({ editForm }) => {
   console.log(editForm);
   return (
     <>
-      {(!resultForm || isEdit) && (
-        <div>
-          <h1>Create account</h1>
-          <form
-            onSubmit={(event) => {
-              event.preventDefault();
+      <div>
+        <h1>Create account</h1>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
 
-              if (password !== passwordConfirm) {
-                setErrorMessage("Mot de passe différents");
-              } else if (!username || !email) {
-                setErrorMessage("information manquantes");
-              } else {
-                setResultForm(true);
-              }
-            }}
-          >
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                placeholder="Sasir votre nom"
-                id="name"
-                value={username}
-                onChange={(event) => {
-                  setUsername(event.target.value);
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                placeholder="Sasir votre Email"
-                id="email"
-                value={email}
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                placeholder="Sasir un mot de passe"
-                id="password"
-                value={password}
-                onChange={(event) => {
-                  setPassword(event.target.value);
-                }}
-              />
-            </div>
-            <div>
-              <label htmlFor="passwordConfirm">Confirm your password</label>
-              <input
-                type="password"
-                placeholder="Confirmer le mot de passe"
-                id="passwordConfirm"
-                value={passwordConfirm}
-                onChange={(event) => {
-                  setPasswordConfirm(event.target.value);
-                }}
-              />
-            </div>
-            <div>
-              {errorMessage && <span>{errorMessage}</span>}
-              <button>Register</button>
-            </div>
-          </form>
-        </div>
-      )}
-      {resultForm && (
-        <StepTwo username={username} email={email} password={password} />
-      )}
+            if (password !== passwordConfirm) {
+              setErrorMessage("Mot de passe différents");
+            } else if (!username || !email) {
+              setErrorMessage("information manquantes");
+            } else {
+              setResultForm(true);
+              <App
+                show={resultForm}
+                username={username}
+                email={email}
+                password={password}
+              />;
+            }
+          }}
+        >
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              placeholder="Sasir votre nom"
+              id="name"
+              value={username}
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              placeholder="Sasir votre Email"
+              id="email"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Sasir un mot de passe"
+              id="password"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="passwordConfirm">Confirm your password</label>
+            <input
+              type="password"
+              placeholder="Confirmer le mot de passe"
+              id="passwordConfirm"
+              value={passwordConfirm}
+              onChange={(event) => {
+                setPasswordConfirm(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            {errorMessage && <span>{errorMessage}</span>}
+            <button>Register</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
